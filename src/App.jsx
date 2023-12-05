bcbe145ef972c81150d2cb3a52b8c08f03efd9ef
EdwardAyo <ayooluomoyiola@gmail.com>
2023-12-05 13:39:58 +0100

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,63 @@ import { React, useState } from "react";
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject";
 import NoProject from "./components/NoProject";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectsId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask]
+      }
+    });
+  }
+
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      }
+    }); 
+  }
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      }
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
       return {
         ...prevState,
         selectedProjectId: null
+      }
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
       }
     });
   }
@@ -34,10 +79,32 @@ function App() {
     });
   }
 
-  let content;
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      }
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+      project => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject
+    project={selectedProject}
+    onDelete={handleDeleteProject}
+    onAddTask={handleAddTask}
+    onDeleteTask={handleDeleteTask}
+    tasks={projectsState.tasks}
+  />;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancelProject={handleCancelAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProject onAddProject={handleStartAddProject} />;
   }
@@ -46,7 +113,9 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <Sidebar 
         onAddProject={handleStartAddProject} 
-        projects={projectsState.projects} 
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
---
+++ src/components/NewTask.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from 'react'
+import Modal from './Modal';
+
+const NewTask = ({ onAdd }) => {
+  const [enteredTask, setEnteredTask] = useState('');
+  const modal = useRef();
+
+  const handleChange = (event) => {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    if (enteredTask.trim() === '') {
+      modal.current.open();
+      setEnteredTask('');
+      return;
+    }
+    onAdd(enteredTask);
+    setEnteredTask('');
+  }
+
+  return (
+    <div className='flex items-center gap-4'>
+      <input
+        type="text"
+        className='w-64 px-2 py-1 rounded-md bg-stone-200'
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button className='text-stone-700 hover:text-stone-950' onClick={handleClick} >Add Task</button>
+      <Modal ref={modal} buttonCaption='Okay'>
+        <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+        <p className='text=stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+        <p className='text=stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+    </div>
+  )
+}
+
+export default NewTask
